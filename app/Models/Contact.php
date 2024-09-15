7400364c7610942d7f2b4dc1854fc6d8d51ed87f
CodeWithRokib <rokibulhasan018722@gmail.com>
2024-09-15 12:53:05 +0600

    contact model function done

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class Contact extends Model
 {
@@ -15,4 +17,27 @@ class Contact extends Model
         'subject',
         'message',
     ];
+
+    public function prepare_data(Request $request)
+    {
+        return [
+            "name" =>$request->input('name'),
+            "email" => $request->input ('email'),
+            "subject" => $request->input('subject'),
+            "message" => $request->input('message'),
+        ];
+    }
+    public function storeContact(Request $request): Builder|Model
+    {
+        return self::query()->create($this->prepare_data($request));
+    }
+    public function updateContact(Request $request, Builder|Model $contact)
+    {
+        return $contact->update($this->prepare_data($request));
+    }
+
+    public function deleteContact(Contact $contact)
+    {
+        return $contact->forceDelete();
+    }
 }
