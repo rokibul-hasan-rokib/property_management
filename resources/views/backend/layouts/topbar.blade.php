f812451a90e8d14ca5a16e48646393b9227caac1
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2024-10-09 23:08:53 +0600

    logout setting

--- resources/views/backend/layouts/topbar.blade.php
+++ resources/views/backend/layouts/topbar.blade.php
@@ -213,9 +213,12 @@
             </li>
 
             <li>
-              <a class="dropdown-item d-flex align-items-center" href="#">
-                <i class="bi bi-box-arrow-right"></i>
-                <span>Sign Out</span>
+                <a class="dropdown-item d-flex align-items-center" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"> <i class="bi bi-box-arrow-right"></i>
+                    <span>Sign Out</span></a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+
               </a>
             </li>
 
