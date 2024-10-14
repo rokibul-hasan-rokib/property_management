472093c359d70f1e6b92492999bd27551e260033
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2024-10-15 00:03:56 +0600

    rentcancel model working

--- app/Models/RentCancel.php
+++ app/Models/RentCancel.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Symfony\Component\Console\Input\Input;
 
 class RentCancel extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    final public  function prepare_data(Request $request){
+        return [
+              "cancel_month"=> $request->input('cancel_month'),
+              'reason' => $request->input('reason'),
+        ];
+    }
+    
 }
