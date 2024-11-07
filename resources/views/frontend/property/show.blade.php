6de13c93f054208e03f468afda68ab1ac63dcdd4
CodeWithRokib <rokibulhasan018722@gmail.com>
2024-11-07 14:41:22 +0600

    modal image slider

--- resources/views/frontend/property/show.blade.php
+++ resources/views/frontend/property/show.blade.php
@@ -34,10 +34,18 @@
                 <div class="col-lg-7">
                     <div class="img-property-slide-wrap">
                         <div class="img-property-slide">
-                            <img src="{{ asset($property->image) }}" alt="Image 1" class="img-fluid thumbnail-image" data-bs-toggle="modal" data-bs-target="#imageModal" data-bs-img="{{ asset($property->image) }}" />
-                            <img src="{{ asset($property->image2) }}" alt="Image 2" class="img-fluid thumbnail-image" data-bs-toggle="modal" data-bs-target="#imageModal" data-bs-img="{{ asset($property->image2) }}" />
-                            <img src="{{ asset($property->image3) }}" alt="Image 3" class="img-fluid thumbnail-image" data-bs-toggle="modal" data-bs-target="#imageModal" data-bs-img="{{ asset($property->image3) }}" />
-                            <img src="{{ asset($property->image4) }}" alt="Image 4" class="img-fluid thumbnail-image" data-bs-toggle="modal" data-bs-target="#imageModal" data-bs-img="{{ asset($property->image4) }}" />
+                            <img src="{{ asset($property->image) }}" alt="Image 1" class="img-fluid thumbnail-image"
+                                data-bs-toggle="modal" data-bs-target="#imageModal"
+                                data-bs-img="{{ asset($property->image) }}" />
+                            <img src="{{ asset($property->image2) }}" alt="Image 2" class="img-fluid thumbnail-image"
+                                data-bs-toggle="modal" data-bs-target="#imageModal"
+                                data-bs-img="{{ asset($property->image2) }}" />
+                            <img src="{{ asset($property->image3) }}" alt="Image 3" class="img-fluid thumbnail-image"
+                                data-bs-toggle="modal" data-bs-target="#imageModal"
+                                data-bs-img="{{ asset($property->image3) }}" />
+                            <img src="{{ asset($property->image4) }}" alt="Image 4" class="img-fluid thumbnail-image"
+                                data-bs-toggle="modal" data-bs-target="#imageModal"
+                                data-bs-img="{{ asset($property->image4) }}" />
                         </div>
                     </div>
                 </div>
@@ -59,7 +67,7 @@
 
                     <div class="d-block agent-box p-5">
                         <div class="img mb-4">
-                            <img src="{{asset('images/person_2-min.jpg')}}" alt="Image" class="img-fluid" />
+                            <img src="{{ asset('images/person_2-min.jpg') }}" alt="Image" class="img-fluid" />
                         </div>
                         <div class="text">
                             <h3 class="mb-0">Alicia Huston</h3>
@@ -89,25 +97,49 @@
         </div>
     </div>
 
-      <!-- Modal to show the larger image -->
-<div class="modal fade" id="imageModal" tabindex="-1" aria-labelledby="imageModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-lg">
-        <div class="modal-content">
-            <div class="modal-body text-center p-0">
-                <!-- Image will be dynamically inserted here -->
-                <img id="modalImage" src="" alt="Large Image" class="img-fluid w-100" />
+    <!-- Modal to show the larger image as a carousel -->
+    <div class="modal fade" id="imageModal" tabindex="-1" aria-labelledby="imageModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+            <div class="modal-content">
+                <div class="modal-body p-0">
+                    <div id="imageCarousel" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="{{ asset($property->image) }}" class="d-block w-100 img-fluid" alt="Image 1">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="{{ asset($property->image2) }}" class="d-block w-100 img-fluid" alt="Image 2">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="{{ asset($property->image3) }}" class="d-block w-100 img-fluid" alt="Image 3">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="{{ asset($property->image4) }}" class="d-block w-100 img-fluid" alt="Image 4">
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#imageCarousel"
+                            data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#imageCarousel"
+                            data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
+
 
     <script>
-        // This script will dynamically load the image into the modal when an image is clicked
-document.querySelectorAll('.thumbnail-image').forEach(image => {
-    image.addEventListener('click', function() {
-        var largeImageSrc = this.getAttribute('data-bs-img');
-        document.getElementById('modalImage').src = largeImageSrc;
-    });
-});
+        document.querySelectorAll('.thumbnail-image').forEach(image => {
+            image.addEventListener('click', function() {
+                var largeImageSrc = this.getAttribute('data-bs-img');
+                document.getElementById('modalImage').src = largeImageSrc;
+            });
+        });
     </script>
 @endsection
