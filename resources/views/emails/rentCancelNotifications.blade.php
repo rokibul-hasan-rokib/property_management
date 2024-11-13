250d19d402175f43c4f4b69e032b455cde558d00
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2024-11-13 23:54:18 +0600

    rental cancel bootstrap cdn add

--- resources/views/emails/rentCancelNotifications.blade.php
+++ resources/views/emails/rentCancelNotifications.blade.php
@@ -2,6 +2,8 @@
 <html>
 <head>
     <title>Rent Cancellation Notification</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </head>
 <body>
     <h1>Rent Cancellation Notice</h1>
@@ -14,5 +16,7 @@
         <li>Status: Cancelled</li>
     </ul>
     <p>If you have any questions, feel free to contact us.</p>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>a
 </body>
 </html>
