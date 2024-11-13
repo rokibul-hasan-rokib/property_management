544f381e5dae36fc36100c6ee680481717af5499
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2024-11-13 23:53:32 +0600

    frontendd settning

--- resources/views/emails/billNotifications.blade.php
+++ resources/views/emails/billNotifications.blade.php
@@ -2,6 +2,8 @@
 <html>
 <head>
     <title>New Bill Notification</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </head>
 <body>
     <h1>New Bill Notification</h1>
@@ -12,6 +14,7 @@
         <li>House: {{ $billDetails['bill_house'] }}</li>
         <li>Electricity Bill: {{ $billDetails['bill_electrity'] }}</li>
         <li>Status: {{ $billDetails['status'] == 1 ? 'Active' : 'Inactive' }}</li>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </ul>
 </body>
 </html>
